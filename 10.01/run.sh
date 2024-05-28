4353dc2b0972cfdc12099673d67c700825183720
forked <30616401+jiejie2004@users.noreply.github.com>
2024-05-29 00:37:11 +0800

    Update run.sh

--- 10.01/run.sh
+++ 10.01/run.sh
@@ -1,1 +1,1 @@
-./pppwn --interface eth2 --fw 1001 --stage1 "/home/root/pppwn/stage1.bin" --stage2 "/home/root/pppwn/stage2.bin" --auto-retry
+./pppwn --interface eth2 --fw 1001 --stage1 "/tmp/pppwn/stage1.bin" --stage2 "/tmp/pppwn/stage2.bin" --auto-retry
